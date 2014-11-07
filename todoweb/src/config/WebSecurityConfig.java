cd63b6e76c5e9123151fde16b41da58ea6c8a3dc
Rodolfo Campos <camposer@gmail.com>
2014-11-07 14:36:28 +0100

    Implementación de configuración en Java Config (securización de método)

--- todoweb/src/config/WebSecurityConfig.java
+++ todoweb/src/config/WebSecurityConfig.java
@@ -3,8 +3,11 @@ package config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -12,6 +15,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
@@ -30,12 +34,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	        	.usernameParameter("j_username")
 	        	.passwordParameter("j_password")
 	        	.and()
-	        .logout()
+	        	.logout()
 	        	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 	        	.logoutSuccessUrl("/index.do")
 	        	.and()
 	        .rememberMe()
 	        	.key("authkey");
+	}
+	
+	// Soporte de métodos securizados
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
 	@Override
