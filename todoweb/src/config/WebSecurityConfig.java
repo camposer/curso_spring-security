f46c753230b8a9572d284d6111d810f36e86f762
Rodolfo Campos <camposer@gmail.com>
2014-11-07 14:08:37 +0100

    Implementación de Spring Security con Java Config

--- todoweb/src/config/WebSecurityConfig.java
+++ todoweb/src/config/WebSecurityConfig.java
@@ -1,23 +1,33 @@
 package config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+//			.csrf().disable()
 	        .authorizeRequests()                                                                
-	            .antMatchers("/admin/**").hasRole("ADMIN")                                      
+	            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")                                  
 	            .and()
 	        .formLogin()
 	        	.loginPage("/login.do")
 	        	.defaultSuccessUrl("/index.do")
+	        	.loginProcessingUrl("/j_spring_security_check")
+	        	.usernameParameter("j_username")
+	        	.passwordParameter("j_password")
 	        	.and()
 	        .logout()
 	        	.logoutUrl("/logout")
@@ -31,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth)
 			throws Exception {
 	    auth
-	        .inMemoryAuthentication()
-	            .withUser("admin").password("password").roles("ADMIN");
-	}
+	        .jdbcAuthentication()
+	            .dataSource(dataSource);
+	  }
 }
