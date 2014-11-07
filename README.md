7b7c2d5ae8a6a6d777872f8f2ce47a22409166ff
Rodolfo Campos <camposer@gmail.com>
2014-11-07 14:45:54 +0100

    README actualizado

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ Curso de Spring Security
 	- http://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/expressions.html
 - Referencing Spring Security configuration within Spring 3.1 Java Config
 	- http://stackoverflow.com/questions/10013996/referencing-spring-security-configuration-within-spring-3-1-java-config
+- Implement custom AuthenticationProvider in Spring Security 2.06
+	- http://stackoverflow.com/questions/8649818/implement-custom-authenticationprovider-in-spring-security-2-06
--- todoweb/src/config/WebSecurityConfig.java
+++ todoweb/src/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 //			.csrf().disable()
-	        .authorizeRequests()                                                                
+			.authorizeRequests()                                                                
 	            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")                                  
 	            .and()
 	        .formLogin()
